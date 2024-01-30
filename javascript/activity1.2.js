4ef0587110f0feae3d8291ed4a63e522a816c39c
IsuruJayamanne <48113477+IsuruJayamanne@users.noreply.github.com>
2024-01-31 00:48:03 +0530

    Update activity1.2.js

---
+++ javascript/activity1.2.js
@@ -0,0 +1,28 @@
+// code
+// method to get array ordered by total
+function orderByTotal(inputArr){
+    // calculate total
+    let newArray = inputArr.map(item => ({
+        amount: item.amount,
+        quantity: item.quantity,
+        Total: item.amount * item.quantity
+    }));
+
+    // sort by total
+    newArray.sort((a, b) => b.Total - a.Total);
+
+    return newArray;
+}
+
+// demo
+
+let salesArray = [
+    { amount: 100, quantity: 2 },
+    { amount: 200, quantity: 10 },
+    { amount: 50, quantity: 10 },
+    { amount: 10, quantity: 20 },
+    { amount: 15, quantity: 30 },
+];
+
+let orderedArr = orderByTotal(salesArray);
+console.log(orderedArr);
