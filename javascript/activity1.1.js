8834125ad617f41702366a291d70db10c52d6061
IsuruJayamanne <48113477+IsuruJayamanne@users.noreply.github.com>
2024-01-31 00:39:37 +0530

    Update activity1.1.js

--- javascript/activity1.1.js
+++ javascript/activity1.1.js
@@ -1,16 +1,19 @@
 // add daysTo method to js Date 
 Date.prototype.daysTo = function(toDate){
     // validate toDate
-    if (!(otherDate instanceof Date)) {
+    if (!(toDate instanceof Date)) {
         throw new Error('Argument should be an instance of Date object');
     }
 
     // calculate date diff
-    let diffMilliseconds = otherDate - this;
-    return Math.floor(diffMilliseconds / (1000 * 60 * 60 * 24));
+    let diffMs = toDate - this;
+    return Math.floor(diffMs / (1000 * 60 * 60 * 24));
 }
 
 // demo
 
-let d1 = new Date('2024-01-21');
-let d2 = new Date('2024-01-23');
+let fromDate = new Date('2024-01-21');
+let toDate = new Date('2024-01-27');
+
+let diff = fromDate.daysTo(toDate)
+console.log(diff)
